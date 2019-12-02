3dc4c4fd404e8c51cdc99f66748e3f469515fce9
suhailsullad <suhail.sullad@gmail.com>
2019-12-02 13:23:28 +0530

    Fixed directoryurl to path

--- src/main.lib/Services/SettingsService.cs
+++ src/main.lib/Services/SettingsService.cs
@@ -90,20 +90,11 @@ namespace PKISharp.WACS.Services
         private void CreateConfigPath()
         {
             var configRoot = "";
-            var directoryUrlIdentfier = DirectoryUri.ToString().Split(new string[] {"/directory/"}, StringSplitOptions.None)[1];
             var userRoot = Client.ConfigurationPath;
 
             if (!string.IsNullOrEmpty(userRoot))
             {
-                if (!string.IsNullOrEmpty(directoryUrlIdentfier))
-                {
-                    _log.Debug("Setting config directory path:{}", directoryUrlIdentfier);
-                    configRoot = string.Join("_", new string[] { userRoot, directoryUrlIdentfier });
-                }
-                else
-                {
-                    configRoot = userRoot;
-                }
+                configRoot = userRoot;
                 // Path configured in settings always wins, but
                 // check for possible sub directories with client name
                 // to keep bug-compatible with older releases that
@@ -127,15 +118,21 @@ namespace PKISharp.WACS.Services
                     var appData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
                     configRoot = Path.Combine(appData, Client.ClientName);
                 }
-                if (!string.IsNullOrEmpty(directoryUrlIdentfier))
-                {
-                    _log.Debug("Setting config directory path:{}", directoryUrlIdentfier);
-                    configRoot = string.Join("_", new string[] { configRoot, directoryUrlIdentfier });
-                }
             }
 
             // This only happens when invalid options are provided 
-            Client.ConfigurationPath = Path.Combine(configRoot, BaseUri.ToString().CleanBaseUri());
+            if (!string.IsNullOrEmpty(DirectoryUri.ToString()))
+            {
+                _log.Debug("Setting config directory path:{}", DirectoryUri.ToString().CleanBaseUri());
+                Client.ConfigurationPath = Path.Combine(configRoot, DirectoryUri.ToString().CleanBaseUri());
+            }
+            else
+            {
+                Client.ConfigurationPath = Path.Combine(configRoot, BaseUri.ToString().CleanBaseUri());
+            }
+
+
+
 
             // Create folder if it doesn't exist yet
             var di = Directory.CreateDirectory(Client.ConfigurationPath);
